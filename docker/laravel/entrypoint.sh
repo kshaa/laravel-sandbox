64a14157c897835bd8aa48cfd3b0c108e5d2e58a
Krisjanis Veinbahs <krisjanis.veinbahs@gmail.com>
2019-04-25 14:47:56 +0300

    Improve php entrypoint for artisan ease-of-use

--- docker/laravel/entrypoint.sh
+++ docker/laravel/entrypoint.sh
@@ -11,7 +11,14 @@ echo "Running at $rundir"
 cd $rundir
 
 # Run appropriate command
-if [ -f "${action_file}" ]
+if [ "${action_command}" == "artisan" ]
+then
+    # Ignore the first given parameter for this script (the bash script name)
+    shift
+
+    # Run artisan w/ given commands
+    bash -c "php artisan \"$*\""
+elif [ -f "${action_file}" ]
 then
     # Ignore the first given parameter for this script (the bash script name)
     shift
